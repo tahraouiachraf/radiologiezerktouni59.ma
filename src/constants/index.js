b5d9a6825168f0a46e4a31da1c5285eb6b71e0b6
tahraouiachraf <tahraouiachraf1@gmail.com>
2025-06-16 16:49:41 +0100

    APP-014

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
 import '../assets/css/Navbar.css';
 import '../assets/css/Hero.css';
 // import '../assets/css/Contact.css';
-import '../assets/css/Footer.css';
+// import '../assets/css/Footer.css';
 import '../assets/css/About.css';
 import '../assets/css/Home.css';
 
