a371e3ab4342ebeecf0eeae54c04a3c16cdf0325
tahraouiachraf <tahraouiachraf1@gmail.com>
2025-06-16 15:51:08 +0100

    APP-011

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import DepartmentsPage from './pages/DepartmentsPage';
 import DoctorsPage from './pages/DoctorsPage';
 import ServicesPage from './pages/ServicesPage';
 import AppointmentPage from './pages/AppointmentPage';
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/departments" element={<DepartmentsPage />} />
         <Route path="/doctors" element={<DoctorsPage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/prendre-rendez-vous" element={<AppointmentPage />} />
